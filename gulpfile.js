85f019a2ac64272422e8ee6c14fb107903783b11
robinvanbuggenhout <robinvanbuggenhout@icloud.com>
2019-05-16 23:49:06 +0200

    added 3th function to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,17 @@
 const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass');
 const nodemon = require('gulp-nodemon');
+const cssnano  = require('gulp-cssnano');
 
 function sass2css(done) {
     src("./public/stylesheets/sass/app.sass")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest("./public/stylesheets/css/"))
+
+    done();
+
+    src("./public/stylesheets/sass/signup.sass")
+        .pipe(sass().on('error', sass.logError))
         .pipe(dest("./public/stylesheets/css/"))
 
     done();
@@ -12,13 +19,27 @@ function sass2css(done) {
 
 function doNodemon(done) {
     nodemon({
-        script: 'index.js'
+        script: './bin/www'
         , ext: 'js pug'
         , env: { 'NODE_ENV': 'development' }
         , done: done
     })
 }
 
+function doCssNano(done) {
+    src('./public/stylesheets/css/app.css')
+        .pipe(cssnano())
+        .pipe(dest('./public/stylesheets/css/dist'));
+    
+    done();
+
+    src('./public/stylesheets/css/signup.css')
+        .pipe(cssnano())
+        .pipe(dest('./public/stylesheets/css/dist'));
+    
+    done();
+}
+
 watch("./public/stylesheets/sass/**/*.sass", sass2css);
 
-module.exports.default = parallel(sass2css, doNodemon);
+module.exports.default = parallel(sass2css, doNodemon, doCssNano);
