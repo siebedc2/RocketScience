f94a3adade4c3aec401538f36ca646060228da83
Siebe <siebedc2@gmail.com>
2019-05-16 20:16:52 +0200

    Added Mongoose model

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,13 +1,4 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const messageSchema = new Schema ({
-    text: {
-        type: String,
-        required: true
-    },
-    user: String
-})
-const Message = mongoose.model('Message', messageSchema);
+const Message = require('../models/Message');
 
 const getAll = (req, res) => {
     Message.find({ }, (err, docs) => {
@@ -24,8 +15,8 @@ const getAll = (req, res) => {
 
 const create = (req, res, next) => {
     let message = new Message();
-    //message.text = "Test Message";
-    message.user = "Siebe";
+    message.text = req.body.text;
+    message.user = req.body.username;
     message.save( (err, doc) => {
         if (err){
             res.json({
