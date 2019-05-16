664177bc5241fac356191fccee512de808575ca2
Siebe <siebedc2@gmail.com>
2019-05-16 21:45:26 +0200

    Added file to signup

---
+++ public/javascripts/signup.js
@@ -0,0 +1,29 @@
+document.querySelector(".submit").addEventListener("click", (e) => {
+
+    let username = document.querySelector(".username").value;
+    let password = document.querySelector(".password").value;
+
+    fetch('/users/signup', {
+        method: "post",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "username": username,
+            "password": password
+        })
+    }).then(response => {
+        return response.json();
+    }).then(json => {
+        if (json.status === "success") {
+            let feedback = document.querySelector(".feedback");
+            feedback.textContent = "Login failed."
+            feedback.classList.remove("hidden");
+            
+            let token = json.data.token;
+            localStorage.setItem("token", token);
+            window.location.href = "/api/v1/messages";
+
+        } 
+    })
+});
