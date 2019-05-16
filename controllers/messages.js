6cae4fdd7bafbfdc8dcb7a6d62d00d98e7a20de6
Siebe <siebedc2@gmail.com>
2019-05-16 20:07:37 +0200

    Added mongoose

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,21 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const messageSchema = new Schema ({
+    text: {
+        type: String,
+        required: true
+    },
+    user: String
+})
+const Message = mongoose.model('Message', messageSchema);
+
 const getAll = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "todos": []
+    Message.find({ }, (err, docs) => {
+        if(!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "todos": docs
+                }
+            });
         }
-    });
+    })    
 }
 
-const create = (req, res) => {
-    res.json({
-        "status": "success",
-        "data": {
-            "todo":{
-                "text": "Learn Node.js"
-            }
+const create = (req, res, next) => {
+    let message = new Message();
+    //message.text = "Test Message";
+    message.user = "Siebe";
+    message.save( (err, doc) => {
+        if (err){
+            res.json({
+                "status": "error",
+                "message": "Could not save this message"
+            });
         }
-    });
+
+        if(!err) {
+            res.json({
+                "status": "success",
+                "data": {
+                    "todo":{
+                        "text": doc
+                    }
+                }
+            });
+        }
+    })
 }
 
 module.exports.getAll = getAll;
